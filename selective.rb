8d88742fea80e08015bc918a7840cc196f5cbfa2
Alan Peterson Carvalho Silva <alanpcsilva@gmail.com>
2016-06-03 20:16:40 +0000

    Remove Alive mutants after counting mutants per operator
    Signed-off-by: Alan Peterson Carvalho Silva <alanpcsilva@gmail.com>

--- selective.rb
+++ selective.rb
@@ -8,15 +8,19 @@ input = File.open(ARGV[0], "r") # Open, read and close the source matix csv file
 csv = CSV.new(input,{:col_sep => ";"}).to_a
 
 header = csv.shift # Remove the first line and stores whitin header
-# Transpose de csv and stores the first column as an array (to avoid cache miss) 
-mutants = csv.transpose.first 
+# Transpose de csv 
+csv_t = csv.transpose
+mutants = csv_t.first # Store the first column as an array (to avoid cache miss) 
+alives = csv_t[-2] # Store the status column as an array (to avoid cache miss) 
+# Get the index of the alive mutants
+alives_idx = alives.map.with_index { |e,i| i if e == "Alive" }.compact
 # Remove '_' and numbers from mutants' names, getting the operator per line
 ops = mutants.map{|m|m.gsub(/[0-9_]/, "")} 
 # Counts up the mutants per operator, and sort ASC by counting stores on a hash
 amounts = Hash[ops.uniq.map{ |o| [o, ops.count(o)] }].sort_by(&:last)
 puts "Mutants by operator: " + amounts.to_h.to_s
 # Creates a string representing the directory to store the results
-result_directory = "selective_results/"+ARGV[0].split("/")[1]+"/" 
+result_directory = "sources/"+ARGV[0].split("/")[1]+"/MS/" 
 FileUtils::mkdir_p(result_directory) # Create the directory if it does not exist
 
 # For each X calls a loop passing x as argument
@@ -27,9 +31,10 @@ X.each do |x|
   File.open(result_directory+"MS_"+x.to_s+".csv", "w") do |out|
     out << header.join(";") << "\n" # Writes the header of the source matrix
     # For each line from CSV, joins the values by ';' delimiter, IF the content
-    #   of the current line from CSV does not contains one of the operators
-    #   chosen to be removed. At the end, remove NULL lines and writes to file
-    csv.map.with_index{|l,i| out << l.join(";") << "\n" if not chosen_ops
-      .include?(ops[i])}.compact
+    #   of the current line from CSV does not contains one of the operators 
+    #   chosen to be removed nor is alive. 
+    #   At the end, remove NULL lines and writes to file
+    csv.map.with_index{|l,i| out << l.join(";") << "\n" if not alives_idx
+      .include?(i) and not chosen_ops.include?(ops[i])}.compact
   end
 end
