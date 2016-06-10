d8c0824e1ccdc12cc63a47aeac07986ff39e97ff
Alan Peterson Carvalho Silva <alanpcsilva@gmail.com>
2016-06-10 19:11:49 +0000

    Filter the exit
    Signed-off-by: Alan Peterson Carvalho Silva <alanpcsilva@gmail.com>

--- run_test_case_selection.sh
+++ run_test_case_selection.sh
@@ -8,6 +8,8 @@ do
     for method in `ls $ProblemsRoot/$problem`
     do
         echo ${ProblemsRoot}${problem}result_list.csv
-        java -jar ${TestRoot}compiled/TestCaseSetSelection.jar ${ProblemsRoot}${problem}${method}result_list.csv ';' true 10
+        java -jar ${TestRoot}compiled/TestCaseSetSelection.jar \
+        ${ProblemsRoot}${problem}${method}result_list.csv ';' \
+        true 10 | grep "Test Cases:" | cut -d':' -f2
     done
 done
