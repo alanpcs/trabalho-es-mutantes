30fe7d892d5c2ece04f142f8d6fcc98d0d975b60
Alan Peterson Carvalho Silva <alanpcsilva@gmail.com>
2016-06-10 19:00:38 +0000

    Cretes script that runs java program to each problem
    Signed-off-by: Alan Peterson Carvalho Silva <alanpcsilva@gmail.com>

---
+++ run_test_case_selection.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+export TestRoot=`pwd`/TestCaseSetSelection/
+echo $TestRoot
+export ProblemsRoot=`pwd`/sources/
+for problem in `ls $ProblemsRoot`
+do
+    echo $problem
+    for method in `ls $ProblemsRoot/$problem`
+    do
+        echo ${ProblemsRoot}${problem}result_list.csv
+        java -jar ${TestRoot}compiled/TestCaseSetSelection.jar ${ProblemsRoot}${problem}${method}result_list.csv ';' true 10
+    done
+done
