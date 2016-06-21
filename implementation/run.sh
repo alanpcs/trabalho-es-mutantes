6588535b157e586baf83d8dda7957d3d22408c7d
Alan Peterson Carvalho Silva <alanpcsilva@gmail.com>
2016-06-21 13:05:21 +0000

    Adds run bash script with clean option
    Signed-off-by: Alan Peterson Carvalho Silva <alanpcsilva@gmail.com>

---
+++ implementation/run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+find ./ -name 'testcases_*' -delete
+find ./ -name 'MS*' -delete
+if [[ $1 != 'clean' ]]
+    then
+    ./selective.rb sources/
+    ./run_test_case_selection 
+    ./get_scores.rb sources/
+fi
